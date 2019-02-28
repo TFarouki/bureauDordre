8ac0f498e01d128a22a62707b0a6cda9dabd5520
catalist <farouki.taoufik@gmail.com>
2019-02-28 07:41:49 +0100

    some tests

--- order.php
+++ order.php
@@ -1,5 +1,4 @@
-<?php
-require_once 'core/init.php'; ?>
+<?php require_once 'core/init.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,136 +6,136 @@ require_once 'core/init.php'; ?>
     <title></title>
   </head>
   <body>
-  <?php
+    <?php
 
-    if (Session::exists("success")) {
-      echo Session::flash("success");
+      if (Session::exists("success")) {
+        echo Session::flash("success");
+      }
+
+
+      $user = new User();
+      if($user->isLoggedIn()){
+    ?>
+    <?php include 'includes/nav.php';?>
+    <div class="container">
+      <h3 class="text-right title"><i class="fa fa-caret-left" aria-hidden="true"></i> <i class="fa fa-book" aria-hidden="true"></i><u> تدبير سجل مكتب الضبط الالكتروني </u></h3>
+      <style>
+        .card-header,.btn-block{
+           margin:0;
+           padding:0;
+        }
+
+        #accordion{
+          margin: 30px;
+        }
+        #accordion .card{
+          border-style: none;
+        }
+        hr{
+          margin-top: 30px;
+          margin-bottom: 30px;
+          height: 2px;
+          background-color: rgba(0,0,0,.05);
+        }
+        .title{
+          margin-top: 40px;
+          margin-bottom: 45px;
+          position: relative;
+          right:-18px;
+          color: rgba(255, 0, 0, 0.5);
+        }
+        .bt-marge{
+          border-bottom: 1px solid rgba(0,0,0,0.5);
+          padding: 0;
+        }
+        .table td,.table th{
+          padding:0;
+        }
+        .controles .btn{
+          width: 180px;
+          margin: 10px;
+        }
+      </style>
+      <div class="text-center controles">
+        <button class="btn btn-success" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><strong><i class="fa fa-pencil-square-o" aria-hidden="true"></i> اضافة تسجيل جديد</strong></button>
+        <button class="btn btn-warning" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><strong><i class="fa fa-search" aria-hidden="true"></i> بحث</strong></button>
+        <button class="btn btn-info" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><strong><i class="fa fa-binoculars" aria-hidden="true"></i> بحث متعدد الوسائط</strong></button>
+        <button class="btn btn-outline-secondary"><strong><i class="fa fa-file-excel-o" aria-hidden="true"></i> تحميل السجل</strong></button>
+      </div>
+      <div id="accordion">
+        <div class="card">
+          <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="" id="rslt">
+        <table class="table table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>John</td>
+              <td>Doe</td>
+              <td>john@example.com</td>
+            </tr>
+            <tr>
+              <td>Mary</td>
+              <td>Moe</td>
+              <td>mary@example.com</td>
+            </tr>
+            <tr>
+              <td>July</td>
+              <td>Dooley</td>
+              <td>july@example.com</td>
+            </tr>
+          </tbody>
+          <tfooter>
+            <tr class="">
+              <td colspan="3" class="text-center bt-marge" ><button class="btn btn-block collapsed btn-default">
+                <strong><i class="fa fa-caret-down" aria-hidden="true"></i> تحميل المزيد <i class="fa fa-caret-down" aria-hidden="true"></i></strong>
+              </button></td>
+            </tr>
+          </tfooter>
+        </table>
+      </div>
+    </div>
+
+
+    <?php
+
+      /*if ($user->hasPermissions("admin")) {
+        echo "<p>You are an Administrator</p>";
+      }
+      if ($user->hasPermissions("modirator")) {
+        echo "<p>You are a Modirator</p>";
+      }*/
+    }else{
+      Redirect::to("login.php");
     }
+    ?>
 
-
-    $user = new User();
-    if($user->isLoggedIn()){
-  ?>
-  <?php include 'includes/nav.php';?>
-  <div class="container">
-    <h3 class="text-right title"><i class="fa fa-caret-left" aria-hidden="true"></i> <i class="fa fa-book" aria-hidden="true"></i><u> تدبير سجل مكتب الضبط الالكتروني </u></h3>
-    <style>
-      .card-header,.btn-block{
-         margin:0;
-         padding:0;
-      }
-
-      #accordion{
-        margin: 30px;
-      }
-      #accordion .card{
-        border-style: none;
-      }
-      hr{
-        margin-top: 30px;
-        margin-bottom: 30px;
-        height: 2px;
-        background-color: rgba(0,0,0,.05);
-      }
-      .title{
-        margin-top: 40px;
-        margin-bottom: 45px;
-        position: relative;
-        right:-18px;
-        color: rgba(255, 0, 0, 0.5);
-      }
-      .bt-marge{
-        border-bottom: 1px solid rgba(0,0,0,0.5);
-        padding: 0;
-      }
-      .table td,.table th{
-        padding:0;
-      }
-      .controles .btn{
-        width: 180px;
-        margin: 10px;
-      }
-    </style>
-    <div class="text-center controles">
-      <button class="btn btn-success" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><strong><i class="fa fa-pencil-square-o" aria-hidden="true"></i> اضافة تسجيل جديد</strong></button>
-      <button class="btn btn-warning" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><strong><i class="fa fa-search" aria-hidden="true"></i> بحث</strong></button>
-      <button class="btn btn-info" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><strong><i class="fa fa-binoculars" aria-hidden="true"></i> بحث متعدد الوسائط</strong></button>
-      <button class="btn btn-outline-secondary"><strong><i class="fa fa-file-excel-o" aria-hidden="true"></i> تحميل السجل</strong></button>
-    </div>
-    <div id="accordion">
-      <div class="card">
-        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-          <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-          <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-          <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </div>
-        </div>
-      </div>
-    </div>
-    <hr />
-    <div class="" id="rslt">
-      <table class="table table-hover">
-        <thead class="thead-dark">
-          <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-          </tr>
-        </tbody>
-        <tfooter>
-          <tr class="">
-            <td colspan="3" class="text-center bt-marge" ><button class="btn btn-block collapsed btn-default">
-              <strong><i class="fa fa-caret-down" aria-hidden="true"></i> تحميل المزيد <i class="fa fa-caret-down" aria-hidden="true"></i></strong>
-            </button></td>
-          </tr>
-        </tfooter>
-      </table>
-    </div>
-  </div>
-
-
-<?php
-
-  /*if ($user->hasPermissions("admin")) {
-    echo "<p>You are an Administrator</p>";
-  }
-  if ($user->hasPermissions("modirator")) {
-    echo "<p>You are a Modirator</p>";
-  }*/
-}else{
-  Redirect::to("login.php");
-}
-?>
-
-</body>
+  </body>
 </html>
--- test.php
+++ test.php
@@ -4,11 +4,91 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <?php include 'includes/include_head.php';?>
+
     <title></title>
   </head>
   <body>
-    <?php
+    <div class="container">
+      <div class="text-right">
 
-    ?>
+        <div class="form-group row">
+          <div class="col-6">
+            <input class="form-control" type="text" placeholder="اسم المرسل" id="example-text-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="col-6">
+            <input class="form-control" type="search" placeholder="اسم المرسل اليه" id="example-search-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="col-6">
+            <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-url-input" class="col-1 col-form-label">URL</label>
+          <div class="col-6">
+            <input class="form-control" type="url" value="https://getbootstrap.com" id="example-url-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-tel-input" class="col-1 col-form-label">Telephone</label>
+          <div class="col-6">
+            <input class="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-password-input" class="col-1 col-form-label">Password</label>
+          <div class="col-6">
+            <input class="form-control" type="password" value="hunter2" id="example-password-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-number-input" class="col-1 col-form-label">Number</label>
+          <div class="col-6">
+            <input class="form-control" type="number" value="42" id="example-number-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-datetime-local-input" class="col-1 col-form-label ">Date and time</label>
+          <div class="col-6">
+            <input class="form-control" type="datetime-local" value="2011-08-19T13:45:00" id="example-datetime-local-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-date-input" class="col-1 col-form-label">Date</label>
+          <div class="col-6">
+            <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-month-input" class="col-1 col-form-label">Month</label>
+          <div class="col-6">
+            <input class="form-control" type="month" value="2011-08" id="example-month-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-week-input" class="col-1 col-form-label">Week</label>
+          <div class="col-6">
+            <input class="form-control" type="week" value="2011-W33" id="example-week-input">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="example-time-input" class="col-1 col-form-label">Time</label>
+          <div class="col-6">
+            <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+          </div>
+        </div>
+        <div class="form-group row">
+        <label for="example-color-input" class="col-1 col-form-label">Color</label>
+        <div class="col-6">
+          <input class="form-control" type="color" value="#563d7c" id="example-color-input">
+        </div>
+      </div>
+
+      </div>
+    </div>
   </body>
 </html>
