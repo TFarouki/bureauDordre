50f2393e95ff3264af714c68982f97a5203ae723
catalist <farouki.taoufik@gmail.com>
2019-03-05 12:58:54 +0100

    old comit

--- order.php
+++ order.php
@@ -66,7 +66,59 @@
         <div class="card">
           <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
             <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              <div class="text-right">
+                <div class="form-group row">
+                  <div class="col-2">
+                    <input type="checkbox" class="toogle-switch" checked id="example-switch-input1" data-width="100" data-toggle="toggle" data-on="وارد" data-off="صادر" data-onstyle="success" data-offstyle="warning">
+                  </div>
+                  <div class="col-5">
+                    <input class="form-control" type="text" placeholder="اسم المرسل" id="example-text-input2">
+                  </div>
+                  <div class="col-5">
+                    <input class="form-control" type="text" placeholder="اسم المرسل اليه" id="example-search-input3">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-4">
+                    <input class="form-control" type="text" placeholder="نوعها" id="example-search-input4">
+                  </div>
+                  <div class="col-4">
+                    <input class="form-control" type="text" placeholder="موضوعها" id="example-search-input5">
+                  </div>
+                  <div class="col-4">
+                    <input class="form-control" type="text" placeholder="مرتبطة بملف" id="example-search-input6">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-4">
+                    <input placeholder="تاريخ الوصول" class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"  id="example-search-input7">
+                  </div>
+                  <div class="col-4">
+                    <input class="form-control" type="file" placeholder="تاريخ الوصول" id="example-search-input8">
+                  </div>
+                  <div class="form-group row">
+                    <div class="col-7">
+                      <div class="dropdown">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                          Dropdown button
+                        </button>
+                        <div class="dropdown-menu">
+                          <a class="dropdown-item" href="#">Link 1</a>
+                          <a class="dropdown-item" href="#">Link 2</a>
+                          <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-1">
+                      <button class="btn btn-primary">إضافة</button>
+                    </div>
+                    <div class="col-4">
+                      <button class="btn btn-danger">الغاء</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
