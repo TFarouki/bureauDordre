cb953dbe9acc1500c1f52d08b020cca9bae0dda3
catalist <farouki.taoufik@gmail.com>
2019-03-12 18:12:00 +0100

    change bg-image upload object is complet

--- test.php
+++ test.php
@@ -17,10 +17,14 @@
       });
 
       $(document).on('change','#customFile',function(){
+
         var file = document.getElementById('customFile');
+        var hidden = $('#fileTmpName').val();
         var lab = document.getElementById('filelab');
         var fullPath = file.value;
         if (fullPath) {
+          $('#progersUpload').show();
+          $('#fileUpload').hide();
             var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
             var filename = fullPath.substring(startIndex);
             if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
@@ -31,19 +35,71 @@
             sizefile = $('#customFile')[0].files[0].size;
             sizefile = sizefile / (1024*1024);
             $('#size').html(sizefile.toFixed(2) + " Mo");
+
+            var property = file.files[0];
+            var form_data = new FormData();
+            form_data.append('hidden',hidden);
+            var file_name = property.name;
+            var ext = file_name.split('.').pop().toLowerCase();
+            var allowExt = ['pdf','doc','docx','bmp','gif','jpeg','jpg','png','tif','tiff','xls','xlsx','mdb'];
+            if($.inArray(ext,allowExt) == -1){
+              alert('invalide file type');
+              $.ajax({
+                url:"upfile.php",
+                method:"POST",
+                data : form_data,
+                contentType : false,
+                processData : false,
+              });
+              $('#fileTmpName').val("");
+              $('#fileUpload').show();
+              $('#progersUpload').hide();
+              $('#filelab').removeClass("bg-success text-white");
+              $('#displayFileName').html(" نسخة الماسح الضوئي");
+            }else{
+              form_data.append('file',property);
+              $.ajax({
+                xhr: function(){
+                    var xhr = new window.XMLHttpRequest();
+                    xhr.upload.addEventListener("progress", function(evt) {
+                        if (evt.lengthComputable) {
+                            var percentComplete = (evt.loaded / evt.total) * 100;
+                            percentComplete = percentComplete.toFixed(2)+'%';
+                            $('#progressbar').width(percentComplete);
+                            $('#percentage').html(percentComplete);
+                            $('#size').width(evt.total);
+                        }
+                   }, false);
+                   return xhr;
+                },
+                url:"upfile.php",
+                method:"POST",
+                data : form_data,
+                contentType : false,
+                processData : false,
+                beforeSend:function(){
+                  $('#progressbar').width('0%')
+                },
+                success:function(data){
+                  $('#fileUpload').show();
+                  $('#progersUpload').hide();
+                  $('#filelab').addClass("bg-success text-white");
+                  $('#displayFileName').html(" " + file_name);
+                  $('#fileTmpName').val(data);
+                  alert(data);
+                }
+              });
+            }
         }
-        $('#progersUpload').show();
-        $('#fileUpload').hide();
       });
     </script>
   </head>
   <body>
     <?php
 
-      if (Session::exists("success")) {
+      if(Session::exists("success")){
         echo Session::flash("success");
       }
-
 
       $user = new User();
       if($user->isLoggedIn()){
@@ -53,7 +109,8 @@
       <div class="form-group row" id="fileUpload">
             <div class="col-3 custom-file mb-3">
               <input type="file" class="custom-file-input" id="customFile" name="filename">
-              <label class="custom-file-label text-left" for="customFile" id="filelab"><i class="fa fa-clone" aria-hidden="true"> نسخة الماسح الضوئي</i></label>
+              <label class="custom-file-label text-center" for="customFile" id="filelab"><i style="font-size:12px;" class="fa fa-clone" aria-hidden="true" id="displayFileName"> نسخة الماسح الضوئي</i></label>
+              <input type="hidden" value="" id="fileTmpName">
             </div>
       </div>
       <div class="form-group row" id="progersUpload">
@@ -61,12 +118,12 @@
             <div class="col-3 small-text">
               <span style="float:right;font:12px;font-weight: bold;" id="fileUploadName"></span><br/>
               <div class="row text-right">
-                <div class="col-3"><span id="percentage">40%</span></div>
+                <div class="col-3"><span id="percentage"></span></div>
                 <div class="col-5"></div>
                 <div class="col-4"><span id="size"></span></div>
               </div>
               <div class="progress " style="height:2px">
-                <div class="progress-bar" style="width:0%;height:2px"></div>
+                <div class="progress-bar" id="progressbar" style="width:0%;height:2px"></div>
               </div>
             </div>
         </div>
---
+++ upfile.php
@@ -0,0 +1,18 @@
+<?php
+  require_once 'core/init.php';
+  if(isset($_POST['hidden']) && $_POST['hidden']!=""){
+    $location = "./FileUpload/tmp/".$_POST['hidden'];
+    unlink($location);
+  }
+  if(isset($_FILES['file']['name'])){
+    $test = explode(".", $_FILES['file']['name']);
+  	$ext = end($test);
+  	$name=Hash::salt(30).".".$ext;
+    $location = "./FileUpload/tmp/".$name;
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+  		echo $name;
+  	}else{
+  		echo "0";
+  	}
+  }
+?>
